731e2dd68e3b8c7e982e79cae511649bf7ac473f
AmroBaniIssa <amro.nedal.issa@gmail.com>
2023-07-30 03:04:32 +0000

    .

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,6 @@
 'use strict';
 require('dotenv').config();
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL, {});
+const {Sequelize,DataTypes}=require('sequelize');
+const sequelize=new Sequelize(process.env.DATABASE_URL,{});
 
-
-module.exports = { sequelize, DataTypes };
+module.exports={sequelize,DataTypes};
--- src/server.js
+++ src/server.js
@@ -1,65 +1,41 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-const basicAuth = require('./middlewars/basic.js')
-const bearer = require('./middlewars/bearer.js');
+const basic = require('./middlewars/basicAuth');
+const bearer = require('./middlewars/bearerAuth');
+const notFoundHandler = require('./handlers/404');
+const errorHandler = require('./handlers/500');
 const bcrypt = require('bcrypt');
-const acl = require('./middlewars/acl.js');
+const users = require('./models/users.model');
 const app = express();
 app.use(express.json());
-const authRouter = express.Router();
-const { Sequelize, DataTypes } = require('sequelize');
-const userModel = require('./models/users.js');
 
-const { users } = require('./models/users.js');
-const sequelize = new Sequelize(process.env.DATABASE_URL, {});
-const user = userModel(sequelize, DataTypes);
 
-authRouter.post('/signup', async (req, res, next) => {
-    try {
-    let userRecord = await users.create(req.body);
-    const output = {
-        user: userRecord,
-        token: userRecord.token
-    };
-    res.status(201).json(output);
-} catch (e) {
-    next(e.message)
+app.post('/signup', async (req, res) => {
+    let username = req.body.username;
+    let hashedPassword = await bcrypt.hash(req.body.password, 5);
+    const record = await users.create({
+        username: username,
+        password: hashedPassword
+    });
+    res.status(201).json(record);
+});
+app.post('/signin', basic, loginHandler);
+
+app.get('/secrets', bearer, secretsHandler)
+
+function loginHandler(req, res) {
+    res.status(200).json(req.user);
+
+
 }
-});
-
-authRouter.post('/signin', basicAuth, (req, res, next) => {
-    const user = {
-        user: req.user,
-        token: req.user.token
-    };
-    res.status(200).json(user);
-});
-
-module.exports = authRouter;
-
-app.get('/myOrders', bearer, ordersHandler);
-
-
-
-app.get('/img', bearer, acl('read'), imageHandler);
-app.post('/img', bearer, acl('create'));
-app.put('/img', bearer, acl('update'));
-app.delete('/img', bearer, acl('delete'), imageDeleteHandler);
-
-
-
-function ordersHandler(req, res) {
-    res.json({
-        'message': 'you can view the orders',
+app.use('*', notFoundHandler);
+app.use(errorHandler)
+function secretsHandler(req, res) {
+    res.status(200).json({
+        'message': 'welcom',
         'user': req.user
     });
 }
 
-function imageHandler(req, res) {
-    res.status(200).json('you have the access');
-}
-function imageDeleteHandler(req, res) {
-    res.status(200).json('you have the access');
-}
 module.exports = app;
